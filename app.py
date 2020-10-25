05ebcd61ac1ec62da6676f8712686352aba1b24e
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-10-24 22:04:39 -0400

    added consumer

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from pykafka import KafkaClient
+
+def get_kafka_client():
+    return KafkaClient(hosts='localhost:9092')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return (render_template('index.html'))
+
+@app.route('/topic/<topicname>')
+def get_message(topicname):
+    client = get_kafka_client
+    def events():
+        for i in client.topics[topicname].get_simple_consumer():
+            yield 'data:{0)\n\n'.format(i.value.decode())
+        return Response(events(), mimetype = 'text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
