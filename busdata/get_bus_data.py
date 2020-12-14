62f98419079bbe9253fa4b41e452640254ced603
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-12-13 20:20:32 -0500

    fixes to bus feed

--- busdata/get_bus_data.py
+++ busdata/get_bus_data.py
@@ -1,16 +1,15 @@
 from google.transit import gtfs_realtime_pb2
-import os
 import requests
 
 #Get live bus data from GoMetro Cincinnati
 class GetbusData:
-    def main():
+    def __init__(self):
         feed = gtfs_realtime_pb2.FeedMessage()
         url = ('http://developer.go-metro.com/TMGTFSRealTimeWebService/vehicle/VehiclePositions.pb')
-        get_feed(feed, url)
+        self.get_feed(feed, url)
 
-
-    def get_feed(feed, url):
+#get the bus feed 
+    def get_feed(self, feed, url):
         response = requests.get(url, allow_redirects=True)
         feed.ParseFromString(response.content)
         with open('output.txt', mode='w') as f:
@@ -18,5 +17,4 @@ class GetbusData:
                 f.write(str(entity))
 
 
-    if __name__ == "__main__":
-        main()
+    
